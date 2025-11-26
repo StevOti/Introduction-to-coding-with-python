00ec0633e3cd3c6fb747fb7d1d99656bc8084041
stephenonyango65@gmail.com <steveotieno5640@gmail.com>
2025-11-26 15:53:52 +0300

    Practicing python questions

--- Notes/practice.py
+++ Notes/practice.py
@@ -370,3 +370,43 @@ while passNumber > 0 and swapMade:
             swapMade = True
     passNumber = passNumber - 1
 print("\nSorted list:\n",fish)
+
+
+# Define a subprogram that will ask the user to pick a low and a high number, and then 
+# generate a random number between those two values and store it in a variable called “comp_num”. 
+# Define another subprogram that will give the instruction “I am thinking of a number…” and then ask the user to 
+# guess the number they are thinking of. 
+# Define a third subprogram that will check to see if the comp_num is the same as the user’s guess. 
+# If it is, it should display the message “Correct, you win”, otherwise it should
+# keep looping, telling the user if they are too low or too high and asking them to guess again until they 
+# guess correctly
+
+import random
+
+def get_num():
+    high = int(input("Enter a high number: "))
+    low = int(input("Enter a low number: "))
+    comp_num = random.randint(low, high)
+    return comp_num
+
+def guess_num():
+    guess = int(input("I am thinking of a number…Guess the number: "))
+    return guess
+
+def check_same(comp_num, guess):
+    try_again = True
+    while try_again == True:
+        if comp_num == guess:
+            print("Correct, you win")
+            try_again = False
+        elif comp_num < guess:
+            guess = int(input("Too low. Try again: "))
+        else:
+            guess = int(input("Too high. Try again: "))
+
+def main():
+    numbers = get_num()
+    input = guess_num()
+    check_same(numbers, input)
+
+main()
--- Notes/test.py
+++
@@ -1,5 +0,0 @@
-list = [[2,3,4], [5, 6, 7], [8, 9, 11]]
-
-row = int(input("Enter a row: "))
-col = int(input("Enter a column: "))
-print(list[row][col])
